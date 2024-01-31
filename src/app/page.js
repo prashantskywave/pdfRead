41aba2d047a89502e9ac07a8926a7fb09c83aefe
prashantskywave <136677987+prashantskywave@users.noreply.github.com>
2024-01-31 10:18:14 +0530

    a

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,16 @@
 import DataTable from "@/components/DataTable";
 import PDFUploader from "@/components/PDFUploader";
 import PdfViewerComponent from "@/components/PDFViewer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// const path = require("path");
+import path from "path";
 
 export default function Home() {
   const [pdfUrl, setPdfUrl] = useState(null);
   const [highlightedAreas, setHighlightedAreas] = useState([]);
   const [filex, setFilex] = useState(null);
   const [tableData, setTableData] = useState(null);
+  const [fileName, setFileName] = useState("");
 
   const purchaseDate = new Date();
   const currentyear = purchaseDate.getFullYear();
@@ -20,22 +23,52 @@ export default function Home() {
 
   const Po_date = `${currentyear}/${currentmonth}/${currentday}/`;
 
-  const handleUpload = (file) => {
-    console.log(
-      ".." +
-        __dirname +
-        "vmsaccounts/storage/uploads/purchaseOrder/" +
-        `${Po_date}`
-    );
+  // const handleUpload = (file) => {
+  //   // console.log(
+  //   //   ".." +
+  //   //     __dirname +
+  //   //     "vmsaccounts/storage/uploads/purchaseOrder/" +
+  //   //     `${Po_date}`
+  //   // );
+  //   const url = URL.createObjectURL(file);
+  //   console.log("file :: ", file);
+  //   console.log("url :: ", url);
+  //   setPdfUrl(url);
+  //   setFilex(file);
+  // };
 
+  useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get("filename");
-    console.log("myParam :: ", myParam);
+    const filePath = `https://vmsaccounts.com/storage/uploads/purchaseOrder/${Po_date}${myParam}`;
 
-    const url = URL.createObjectURL(file);
-    setPdfUrl(url);
-    setFilex(file);
-  };
+    // console.log("filePath :: ", filePath);
+    fetch(filePath)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch file (${response.status})`);
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        // Extract file name from the URL
+        const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+        setFileName(fileName?.split(".")[0]);
+
+        // Create a File object with the extracted name
+        const file = new File([blob], fileName, { type: "application/pdf" });
+
+        // Now you can use the 'file' object as needed
+        // console.log("file :: ", file);
+
+        // For example, create a URL for the File
+        const url = URL.createObjectURL(file);
+        // console.log("url :: ", url)running;
+        setPdfUrl(url);
+        setFilex(file);
+      })
+      .catch((error) => console.error("Error fetching file:", error));
+  }, []);
 
   const handleAreaSelect = (area) => {
     setHighlightedAreas([...highlightedAreas, area]);
@@ -43,7 +76,7 @@ export default function Home() {
 
   return (
     <div>
-      <PDFUploader onUpload={handleUpload} />
+      {/* <PDFUploader onUpload={handleUpload} /> */}
       {!tableData && pdfUrl && (
         <PdfViewerComponent
           pdfUrl={pdfUrl}
@@ -52,7 +85,7 @@ export default function Home() {
           setTableData={setTableData}
         />
       )}
-      {tableData && <DataTable data={tableData} />}
+      {tableData && <DataTable data={tableData} fileName={fileName} />}
     </div>
   );
 }
--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-const formData = new FormData();
-
-const DataTable = ({ data }) => {
+const DataTable = ({ data, fileName }) => {
   const [tableData, setTableData] = useState(data);
+  const [notification, setNotification] = useState(null);
+
+  const showNotification = (message, type) => {
+    setNotification({ message, type });
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000); // Close the notification after 3 seconds
+  };
 
   const handleCellEdit = (rowIndex, cellIndex, value) => {
-    // console.log(rowIndex, cellIndex, value);
     const updatedData = tableData.data.map((row, rIndex) =>
       rIndex === rowIndex
         ? row
@@ -14,6 +19,17 @@ const DataTable = ({ data }) => {
             .join(",")
         : row
     );
+    setTableData({ data: updatedData });
+  };
+
+  const handleHeaderEdit = (headerIndex, value) => {
+    const updatedHeaders = tableData.data[0]
+      ?.split(",")
+      .map((header, index) =>
+        index === headerIndex ? value.toUpperCase() : header.toUpperCase()
+      );
+    const updatedData = [updatedHeaders.join(","), ...tableData.data.slice(1)];
+    showNotification(`${value.toUpperCase()} is Updating`, "warning");
     setTableData({ data: updatedData });
   };
 
@@ -30,45 +46,53 @@ const DataTable = ({ data }) => {
     return rowData;
   };
 
+  const handleDeleteRow = (rowIndex) => {
+    const updatedData = tableData.data.filter(
+      (row, rIndex) => rIndex !== rowIndex + 1
+    );
+    setTableData({ data: updatedData });
+  };
+
   const handleTableCorrectClick = () => {
     const headers = tableData.data[0]?.split(",") || [];
+    const barcodeIndex = headers.indexOf("BARCODE");
+
+    if (barcodeIndex === -1) {
+      showNotification(
+        "Error: Barcode column not found in the table.",
+        "error"
+      );
+      return;
+    }
     const convertedData = tableData.data
       .slice(1)
       .map((row) => convertRowToObject(row, headers));
 
-    const apiUrl = `https://devvms.vebasatrading.com/api/getPoData`;
-    const formData = new FormData();
-    formData.append("poData", JSON.stringify(convertedData));
+    const apiUrl = `http://localhost:6969/api/getPoData`;
+    // const apiUrl = `https://nodeapi.vmsaccounts.com/api/getPoData`;
 
     const response = async (convertedData) => {
       try {
-        console.log(formData.get("poData"));
-        const res = await fetch(apiUrl, {
+        const fetchResponse = await fetch(apiUrl, {
           method: "POST",
-          body: JSON.stringify({ poData: convertedData }), // Wrap the data in an object
+          body: JSON.stringify({ poData: convertedData, fileName }),
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
         });
-
-        if (!res.ok) {
-          throw new Error(`HTTP error! Status: ${res.status}`);
-        }
-
-        console.log("Res :: ", res);
-
-        const data = await res.json();
-        // Do something with the data
+        const data = await fetchResponse.json();
         console.log(data);
+        showNotification("Table data successfully submitted!", "success");
       } catch (error) {
-        // Handle errors
-        console.error("Error fetching data:", error.message);
+        showNotification(
+          `Error submitting table data:: ${error.message}`,
+          "error"
+        );
       }
     };
 
-    // Call the function to initiate the API request
     response(convertedData);
-    console.log("Data stored in localStorage:", convertedData);
+    // console.log("convertedData", convertedData);
   };
 
   return (
@@ -92,36 +116,68 @@ const DataTable = ({ data }) => {
                       padding: "8px",
                       textAlign: "left",
                       backgroundColor: "#000",
+                      color: "#fff",
+                      textTransform: "uppercase",
                     }}
+                    // contentEditable={true}
+                    // onBlur={(e) =>
+                    //   handleHeaderEdit(index, e.currentTarget.innerText)
+                    // }
                   >
-                    <select
+                    <input
+                      type="text"
                       style={{
-                        background: "transparent",
+                        margin: "0",
+                        padding: "10px",
+                        textTransform: "uppercase",
+                        backgroundColor: "#000",
                         border: "none",
                         color: "#fff",
+                        outline: "none",
                       }}
-                      defaultValue={header}
-                    >
-                      {tableData.data[0]
-                        ?.split(",")
-                        .map((option, optionIndex) => (
-                          <option
-                            style={{ background: "black", color: "#fff" }}
-                            key={optionIndex}
-                            value={option}
-                          >
-                            {option}
-                          </option>
-                        ))}
-                    </select>
+                      value={header}
+                      onChange={(e) => handleHeaderEdit(index, e.target.value)}
+                    />
                   </th>
                 ))}
+                <th
+                  style={{
+                    border: "1px solid #ddd",
+                    padding: "8px",
+                    textAlign: "left",
+                    backgroundColor: "#000",
+                    color: "#fff",
+                    textTransform: "uppercase",
+                  }}
+                >
+                  Actions
+                </th>
               </tr>
             </thead>
             <tbody>
               {tableData.data?.slice(1).map((row, rowIndex) => (
                 <tr key={rowIndex}>
                   {row.split(",").map((cell, cellIndex) => (
+                    // <td
+                    //   key={cellIndex}
+                    //   style={{
+                    //     border: "1px solid #ddd",
+                    //     padding: "8px",
+                    //     textAlign: "left",
+                    //   }}
+                    // >
+                    //   <div
+                    //     contentEditable
+                    //     onChange={(e) =>
+                    //       handleCellEdit(
+                    //         rowIndex + 1,
+                    //         cellIndex,
+                    //         e.currentTarget.innerText
+                    //       )
+                    //     }
+                    //     dangerouslySetInnerHTML={{ __html: cell }}
+                    //   />
+                    // </td>
                     <td
                       key={cellIndex}
                       style={{
@@ -130,19 +186,48 @@ const DataTable = ({ data }) => {
                         textAlign: "left",
                       }}
                     >
-                      <div
-                        contentEditable
-                        onBlur={(e) =>
+                      <input
+                        style={{
+                          border: "none",
+                          outline: "none",
+                        }}
+                        type="text"
+                        value={cell}
+                        onChange={(e) =>
                           handleCellEdit(
                             rowIndex + 1,
                             cellIndex,
-                            e.currentTarget.innerText
+                            e.target.value
                           )
                         }
-                        dangerouslySetInnerHTML={{ __html: cell }}
                       />
                     </td>
                   ))}
+                  <td
+                    style={{
+                      border: "1px solid #ddd",
+                      padding: "8px",
+                      textAlign: "center",
+                    }}
+                  >
+                    {/* <button > */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 48 48"
+                      width="25"
+                      height="25"
+                      onClick={() => handleDeleteRow(rowIndex)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <path
+                        d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4V14H12v24zM38 8h-7l-2-2H19l-2 2h-7v4h28V8z"
+                        fill="#f00"
+                        class="color000 svgShape"
+                      ></path>
+                      <path fill="none" d="M0 0h48v48H0z"></path>
+                    </svg>
+                    {/* </button> */}
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -152,8 +237,39 @@ const DataTable = ({ data }) => {
       <div>
         <button onClick={handleTableCorrectClick}>Table is Correct</button>
       </div>
+      {/* Custom Notification */}
+      {notification && (
+        <div
+          style={{
+            position: "fixed",
+            top: "20px",
+            right: "20px",
+            padding: "10px",
+            backgroundColor: getNotificationColor(notification.type),
+            color: "#fff",
+            borderRadius: "5px",
+            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+          }}
+        >
+          {notification.message}
+        </div>
+      )}
     </div>
   );
 };
 
 export default DataTable;
+
+// Helper function to get the notification background color based on the type
+const getNotificationColor = (type) => {
+  switch (type) {
+    case "error":
+      return "red";
+    case "success":
+      return "green";
+    case "warning":
+      return "orange";
+    default:
+      return "gray";
+  }
+};
